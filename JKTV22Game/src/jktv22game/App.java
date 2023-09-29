6dc783494888145133ff3a3e9be2a54ecce64183
Feitanex <146435854+Feitanex@users.noreply.github.com>
2023-09-29 13:39:13 +0300

    Add files via upload

---
+++ JKTV22Game/src/jktv22game/App.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jktv22game;
+
+/**
+ *
+ * @author pupil
+ */
+public class App {
+
+    void run() {
+         
+    }
+    
+}
